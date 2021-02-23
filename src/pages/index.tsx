04a83e53ccdaffa25767da307a5b1702f72fb47e
Lucas Duarte <66844060+Lucas-Duarte-dev@users.noreply.github.com>
2021-02-23 13:40:29 -0300

    finalizado o segundo dia da nlw

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { CountDown } from '../components/CountDown';
+import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile';
+
+import Head from 'next/head'
+
+import styles from '../styles/components/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  );
+}
